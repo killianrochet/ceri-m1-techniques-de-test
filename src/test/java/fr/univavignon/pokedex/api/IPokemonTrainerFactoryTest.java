5cc8eaa42c2771631f8a3a81b5b1871e1d4f05b6
Killian <rochetkillianpro@icloud.com>
2022-05-09 00:20:42 +0200

    Fin TP3 Ajout test Pokedex / TrainerFactory

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -0,0 +1,35 @@
+package fr.univavignon.pokedex.api;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class IPokemonTrainerFactoryTest {
+
+    IPokemonTrainerFactory mockPokemonTrainerFactory=mock(IPokemonTrainerFactory.class);
+    IPokedexFactory mockPokedexFactory = mock(IPokedexFactory.class);
+    IPokedex mockPokedex = mock(IPokedex.class);
+    PokemonTrainer trainer = new PokemonTrainer("Killian",Team.INSTINCT, mockPokedex);
+
+    @Before
+    public void setUp(){
+        when(mockPokemonTrainerFactory.createTrainer("Killian",Team.INSTINCT, mockPokedexFactory)).thenReturn(trainer);
+    }
+
+    @Test
+    public void testCreateTrainer() {
+        assertEquals("Killian", mockPokemonTrainerFactory.createTrainer("Killian",Team.INSTINCT, mockPokedexFactory).getName());
+    }
+
+    @Test
+    public void testCreateTrainerGetTeam() {
+        assertEquals(Team.INSTINCT, mockPokemonTrainerFactory.createTrainer("Killian",Team.INSTINCT, mockPokedexFactory).getTeam());
+    }
+
+    @Test
+    public void testCreateTrainerGetPokedex() {
+        assertNotNull(mockPokemonTrainerFactory.createTrainer("Killian",Team.INSTINCT, mockPokedexFactory).getPokedex());
+    }
+}
