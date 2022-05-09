4e0b8e14cc3483e71434cd7fbf383893231e5716
Killian <rochetkillianpro@icloud.com>
2022-05-09 09:55:31 +0200

    Fin TP3 Ajout test Pokedex / TrainerFactory

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -9,16 +9,13 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
-public class IPokemonMetadataProviderTest  extends TestCase {
-
+public class IPokemonMetadataProviderTest extends TestCase {
     IPokemonMetadataProvider metaData = mock(IPokemonMetadataProvider.class);
-
-    PokemonMetadata bulbizarre=new PokemonMetadata(0,"Bulbizarre",126,126,90);
-    PokemonMetadata aquali=new PokemonMetadata(133,"Aquali",186,168,260);
-
+    PokemonMetadata bulbizarre = new PokemonMetadata(0,"Bulbizarre",126,126,90);
+    PokemonMetadata aquali = new PokemonMetadata(133,"Aquali",186,168,260);
 
     @Before
-    public void init(){
+    public void setUp(){
         try {
             when(metaData.getPokemonMetadata(0)).thenReturn(bulbizarre);
             when(metaData.getPokemonMetadata(133)).thenReturn(aquali);
@@ -36,7 +33,7 @@ public class IPokemonMetadataProviderTest  extends TestCase {
             assertEquals(126, testMetaData.getAttack());
             assertEquals(126, testMetaData.getDefense());
             assertEquals(90, testMetaData.getStamina());
-        }catch (PokedexException e){
+        } catch (PokedexException e){
             e.printStackTrace();
         }
     }
